96550a9333a7360574f98afdd90f3a3311d86315
Mike Ona <mona@ucsd.edu>
2018-02-21 16:48:04 -0800

    Lab 7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,14 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
-}
+
+	$('.likeBtn').click(function(){
+		console.log("test");
+		//ga("send", "event", 'like', 'click'); This did not work. 
+		//I am using code below from https://developers.google.com/analytics/devguides/collection/gtagjs/events
+    	gtag('event', 'click', {
+  			'event_category': 'like'
+});
+	});
+};
+
